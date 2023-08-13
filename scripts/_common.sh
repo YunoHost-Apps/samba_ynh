fccfac2e7b89231d35e562552584d36ae92a56af
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-13 12:38:51 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="samba"
+#REMOVEME? pkg_dependencies="samba"
 
 #=================================================
 # PERSONAL HELPERS
